56a05310e633f94647440b99608bb3f00fbec3ea
Shreya Tiwari <shreyatiwari0801@gmail.com>
2026-05-28 17:38:28 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -3,7 +3,7 @@ from qdrant_client import QdrantClient
 import os, time
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv() 
 client_ai = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 db = QdrantClient(path="./qdrant_data")
 COLLECTION = "medical_books"
